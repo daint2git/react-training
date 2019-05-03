32af283264e3419cddb941ed4b123591b101a9cd
daint2git <daint2dev@gmail.com>
2019-05-03 22:53:36 +0700

    update test

--- react-mobx/src/stores/RepoStore/index.test.js
+++ react-mobx/src/stores/RepoStore/index.test.js
@@ -1,7 +1,7 @@
-import mockAxios from 'axios'
+import axios from 'axios'
 import { RepoStore } from './'
 
-jest.mock('axios')
+// jest.mock('axios')
 
 describe('RepoStore', () => {
   let store
@@ -10,14 +10,43 @@ describe('RepoStore', () => {
     store = new RepoStore()
   })
 
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
+  // afterEach(() => {
+  //   jest.resetAllMocks()
+  // })
 
   test('should be equal the initial state', () => {
     expect(store.repos).toEqual([])
     expect(store.repoCount).toBe(0)
   })
+
+  // test('should handle fetchRepos (1)', async () => {
+  //   const mockData = [
+  //     {
+  //       id: 183929683,
+  //       html_url: 'https://github.com/daint2git/react-jest-enzyme',
+  //     },
+  //   ]
+  //   axios.get.mockResolvedValue({ data: mockData })
+  //   await store.fetchRepos()
+  //   expect(axios.get).toHaveBeenCalled()
+  //   expect(axios.get).toHaveBeenCalledTimes(1)
+  //   expect(axios.get).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
+  //   expect(store.repoCount).toBe(1)
+  //   expect(store.repos).toEqual([
+  //     {
+  //       id: mockData[0].id,
+  //       htmlUrl: mockData[0].html_url,
+  //     },
+  //   ])
+  // })
+
+  // test('should handle fetchRepos (2)', async () => {
+  //   axios.get.mockResolvedValue({ data: {} })
+  //   await store.fetchRepos()
+  //   expect(axios.get).toHaveBeenCalled()
+  //   expect(axios.get).toHaveBeenCalledTimes(1)
+  //   expect(axios.get).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
+  // })
 
   test('should handle fetchRepos (1)', async () => {
     const mockData = [
@@ -26,11 +55,12 @@ describe('RepoStore', () => {
         html_url: 'https://github.com/daint2git/react-jest-enzyme',
       },
     ]
-    mockAxios.get.mockResolvedValue({ data: mockData })
+    const mockGet = jest.spyOn(axios, 'get')
+    mockGet.mockResolvedValue({ data: mockData })
     await store.fetchRepos()
-    expect(mockAxios.get).toHaveBeenCalled()
-    expect(mockAxios.get).toHaveBeenCalledTimes(1)
-    expect(mockAxios.get).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
+    expect(mockGet).toHaveBeenCalled()
+    expect(mockGet).toHaveBeenCalledTimes(1)
+    expect(mockGet).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
     expect(store.repoCount).toBe(1)
     expect(store.repos).toEqual([
       {
@@ -38,13 +68,16 @@ describe('RepoStore', () => {
         htmlUrl: mockData[0].html_url,
       },
     ])
+    mockGet.mockRestore()
   })
 
   test('should handle fetchRepos (2)', async () => {
-    mockAxios.get.mockResolvedValue({ data: {} })
+    const mockGet = jest.spyOn(axios, 'get')
+    mockGet.mockResolvedValue({ data: {} })
     await store.fetchRepos()
-    expect(mockAxios.get).toHaveBeenCalled()
-    expect(mockAxios.get).toHaveBeenCalledTimes(1)
-    expect(mockAxios.get).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
+    expect(mockGet).toHaveBeenCalled()
+    expect(mockGet).toHaveBeenCalledTimes(1)
+    expect(mockGet).toHaveBeenCalledWith('https://api.github.com/users/daint2git/repos')
+    mockGet.mockRestore()
   })
 })
